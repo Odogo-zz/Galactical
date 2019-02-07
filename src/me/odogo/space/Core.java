a44c42a7b3146a42da5bc65ef0006bea5790458b
Odogo <odogogaming514@gmail.com>
2019-02-06 20:36:44 -0600

    Main Frame v1

---
+++ src/me/odogo/space/Core.java
@@ -0,0 +1,90 @@
+package me.odogo.space;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.logging.Level;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import net.milkbowl.vault.chat.Chat;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.permission.Permission;
+
+public class Core extends JavaPlugin {
+
+	public ArrayList<Player> astronauts = new ArrayList<Player>();
+	public HashMap<Player, Boolean> astronautRadio = new HashMap<Player, Boolean>();
+
+	private static Economy econ = null;
+	private static Permission perms = null;
+	private static Chat chat = null;
+
+	@Override
+	public void onEnable() {
+		registerConfigs();
+
+		if(getConfig().getBoolean("use-vault-intergration")) {
+
+			if (!setupEconomy() ) {
+				getLogger().severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
+				getLogger().severe("Please disable the vault integration in the configuration of this plugin.");
+				getServer().getPluginManager().disablePlugin(this);
+				return;
+			}
+			setupPermissions();
+			setupChat();
+
+		} else { this.getLogger().log(Level.INFO, "'use-vault-intergration' was set to false. Skipping Vault Intergration."); }
+
+		registerEvents();
+	}
+
+	private void registerEvents() {
+
+	}
+
+	private void registerConfigs() {
+
+		getConfig().addDefault("use-vault-intergration", true);
+		getConfig().options().copyDefaults(true);
+		saveConfig();
+
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+
+	//Vault Integration
+	//Copyright (C) 2011-2018 Morgan Humes morgan@lanaddict.com for the next 3 methods.
+
+	private boolean setupEconomy() {
+		if (getServer().getPluginManager().getPlugin("Vault") == null) {
+			return false;
+		}
+		RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+		if (rsp == null) {
+			return false;
+		}
+		econ = rsp.getProvider();
+		return econ != null;
+	}
+
+	private boolean setupChat() {
+		RegisteredServiceProvider<Chat> rsp = getServer().getServicesManager().getRegistration(Chat.class);
+		chat = rsp.getProvider();
+		return chat != null;
+	}
+
+	private boolean setupPermissions() {
+		RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
+		perms = rsp.getProvider();
+		return perms != null;
+	}
+
+	//The rest of the methods below are now by me.
+
+}
---
+++ src/me/odogo/space/util/Actionbar.java
@@ -0,0 +1,72 @@
+package me.odogo.space.util;
+
+import java.util.ArrayList;
+
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+import net.minecraft.server.v1_12_R1.IChatBaseComponent.ChatSerializer;
+import net.minecraft.server.v1_12_R1.PacketPlayOutTitle;
+import net.minecraft.server.v1_12_R1.PacketPlayOutTitle.EnumTitleAction;
+
+public class Actionbar {
+
+	private PacketPlayOutTitle packet;
+	private String message;
+	private Player player;
+
+	public Actionbar(String message) {
+		this.message = message;
+
+		packet = new PacketPlayOutTitle(EnumTitleAction.ACTIONBAR, ChatSerializer.a("{\"text\":\"" + this.message + "\"}"), 0, 0, 0);
+	}
+
+	public Actionbar(String message, Player player) {
+		this.player = player;
+		this.message = message;
+	}
+
+	public void sendActionbar(Player player) {
+		((CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
+	}
+
+	public void sendActionbar() {
+		if(player == null) {
+			NullPointerException e = new NullPointerException("Player cannot be null.");
+			e.printStackTrace();
+			return;
+		}
+
+		((CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
+	}
+
+	public void sendActionbarToPlayers(ArrayList<Player> player) {
+		for(Player players : player) {
+			((CraftPlayer) players).getHandle().playerConnection.sendPacket(packet);
+		}
+	}
+
+	public void sendActionbarToPlayersOnline() {
+		for(Player players : Bukkit.getOnlinePlayers()) {
+			((CraftPlayer) players).getHandle().playerConnection.sendPacket(packet);
+		}
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Player getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(Player player) {
+		this.player = player;
+	}
+
+}
