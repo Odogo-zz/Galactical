70eff9565d690e96d7455891b9805ea6fa8babd5
Odogo <31525656+Odogo@users.noreply.github.com>
2019-02-25 14:45:33 -0600

    Creation of Forgery.java
    Another copy of Crafting.java, just changed a couple values.

---
+++ src/me/odogo/space/events/stations/Forgery.java
@@ -0,0 +1,56 @@
+package me.odogo.space.events.stations;
+
+public class Forgery implements Listener {
+	
+	private Core plugin = Core.getPlugin(Core.class);
+	private final String prefix = plugin.prefix;
+	
+	@EventHandler
+	public void onStationCraft(PlayerInteractionEvent e) {
+		Player player = e.getPlayer();
+		
+		if(e.getAction != Action.RIGHT_CLICK_BLOCK) {
+			return;
+		}
+		
+		Block block = e.getBlock();
+		World world = block.getWorld();
+		Location bLoc = block.getLocation();
+		
+		if(block.getType() != Material.ANVIL) {
+			return;
+		}
+		
+		Block bottom = new Location(world, bLoc.getX(), bLoc.getY() - 1, bLoc.getZ()).getBlock();
+		Block aboveOne = new Location(world, bLoc.getX(), bLoc.getY() + 1, bLoc.getZ()).getBlock();
+		
+		if(bottom.getType() == Material.DROPPER && aboveOne.getType() == Material.ANVIL) {
+				
+			Dropper dropper = (Dropper) bottom;
+			ItemStack[] shape = droppper.getInventory().getContains();
+			
+		}
+	}
+	
+	@EventHandler
+	public void onStationCreation(BlockPlaceEvent e) {
+		Player player = e.getPlayer();
+		
+		Block block = e.getBlock();
+		World world = block.getWorld();
+		Location bLoc = block.getLocation();
+		
+		if(block.getType() == Material.ANVIL) {
+			
+			Block bottomOne = new Location(world, bLoc.getX(), bLoc.getY() - 1, bLoc.getZ()).getBlock();
+			Block bottomTwo = new Location(world, bLoc.getX(), bLoc.getY() - 2, bLoc.getZ()).getBlock();
+			
+			if(bottomOne.getType() == Material.ANVIL && bottomTwo.getType() == Material.DROPPER) {
+				
+				player.sendMessage(prefix + ChatColor.GREEN + "Successfully created a Crafting Station!");
+				player.playSound(bLoc, Sound.BLOCK_ANVIL_USE, 1, 1);
+				
+			}
+		}
+	}
+}
