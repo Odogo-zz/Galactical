228517ad4cb46f40aafe2fa403b9357e6591b43f
Odogo <odogogaming514@gmail.com>
2019-02-25 20:15:01 -0600

    Imports and Spelling Errors
    Went through the ckasses and updated the imports and spelling.

--- src/me/odogo/space/Core.java
+++ src/me/odogo/space/Core.java
@@ -2,8 +2,10 @@ package me.odogo.space;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.UUID;
 import java.util.logging.Level;
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -16,6 +18,8 @@ public class Core extends JavaPlugin {
 
 	public ArrayList<Player> astronauts = new ArrayList<Player>();
 	public HashMap<Player, Boolean> astronautRadio = new HashMap<Player, Boolean>();
+
+	public final String prefix = ChatColor.GRAY + "-[ " + ChatColor.AQUA + "Galactical" + ChatColor.GRAY + " ]- " + ChatColor.RESET;
 
 	private static Economy econ = null;
 	private static Permission perms = null;
@@ -86,52 +90,54 @@ public class Core extends JavaPlugin {
 	}
 
 	//The rest of the methods below are now by me.
-	
+
 	public void addAstronaut(Player player) {
-		
-		c
-		
-		astronauts.add(player);
-	}
-	
-	public void removeAstronaut(Player player) {
-		
-		astronauts.remove(player);
-		
-	}
-	
-	public ArrayList<Player> getAstronauts() {
-		return astronauts;
-	}
-	
-	public Player getAstronautByName(String idientifier) {
-		for(Player astronaut : astronauts) {
-			if(astronaut.getName().equals(idienetifier)) {
-				return astronaut;
-			}
-		}
-	}
-	
-	public Player getAstronautByUUID(UUID idientifier) {
-		for(Player astronaut : astronauts) {
-			if(astronaut.getUniqueID() == idientifier) {
-				return astronaut;
-			}
-		}
-	}
-	
-	public HashMap<Player, Boolean> getAstronautRadio() {
-		return astronautRadio;
-	}
-	
-	public void setAstronautRadio(Player player, boolean state) {
-		
 		if(player == null) {
 			NullPointerException e = new NullPointerException("The player cannot be null");
 			e.printStackTrace();
 			return;
 		}
-		
+
+		astronauts.add(player);
+	}
+
+	public void removeAstronaut(Player player) {
+		astronauts.remove(player);
+	}
+
+	public ArrayList<Player> getAstronauts() {
+		return astronauts;
+	}
+
+	public Player getAstronautByName(String idientifier) {
+		for(Player astronaut : astronauts) {
+			if(astronaut.getName().equals(idientifier)) {
+				return astronaut;
+			}
+		}
+		return null;
+	}
+
+	public Player getAstronautByUUID(UUID idientifier) {
+		for(Player astronaut : astronauts) {
+			if(astronaut.getUniqueId() == idientifier) {
+				return astronaut;
+			}
+		}
+		return null;
+	}
+
+	public HashMap<Player, Boolean> getAstronautRadio() {
+		return astronautRadio;
+	}
+
+	public void setAstronautRadio(Player player, boolean state) {
+		if(player == null) {
+			NullPointerException e = new NullPointerException("The player cannot be null");
+			e.printStackTrace();
+			return;
+		}
+
 		astronautRadio.put(player, state);
 	}
 }
--- src/me/odogo/space/events/stations/Forgery.java
+++ src/me/odogo/space/events/stations/Forgery.java
@@ -1,55 +1,72 @@
 package me.odogo.space.events.stations;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.block.Dropper;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+
+import me.odogo.space.Core;
+
 public class Forgery implements Listener {
-	
+
 	private Core plugin = Core.getPlugin(Core.class);
 	private final String prefix = plugin.prefix;
-	
+
 	@EventHandler
-	public void onStationCraft(PlayerInteractionEvent e) {
+	public void onStationCraft(PlayerInteractEvent e) {
 		Player player = e.getPlayer();
-		
-		if(e.getAction != Action.RIGHT_CLICK_BLOCK) {
+
+		if(e.getAction() != Action.RIGHT_CLICK_BLOCK) {
 			return;
 		}
-		
-		Block block = e.getBlock();
+
+		Block block = e.getClickedBlock();
 		World world = block.getWorld();
 		Location bLoc = block.getLocation();
-		
+
 		if(block.getType() != Material.ANVIL) {
 			return;
 		}
-		
+
 		Block bottom = new Location(world, bLoc.getX(), bLoc.getY() - 1, bLoc.getZ()).getBlock();
 		Block aboveOne = new Location(world, bLoc.getX(), bLoc.getY() + 1, bLoc.getZ()).getBlock();
-		
+
 		if(bottom.getType() == Material.DROPPER && aboveOne.getType() == Material.ANVIL) {
-				
+
 			Dropper dropper = (Dropper) bottom;
-			ItemStack[] shape = droppper.getInventory().getContains();
-			
+			ItemStack[] shape = dropper.getInventory().getContents();
+
 		}
 	}
-	
+
 	@EventHandler
 	public void onStationCreation(BlockPlaceEvent e) {
 		Player player = e.getPlayer();
-		
+
 		Block block = e.getBlock();
 		World world = block.getWorld();
 		Location bLoc = block.getLocation();
-		
+
 		if(block.getType() == Material.ANVIL) {
-			
+
 			Block bottomOne = new Location(world, bLoc.getX(), bLoc.getY() - 1, bLoc.getZ()).getBlock();
 			Block bottomTwo = new Location(world, bLoc.getX(), bLoc.getY() - 2, bLoc.getZ()).getBlock();
-			
+
 			if(bottomOne.getType() == Material.ANVIL && bottomTwo.getType() == Material.DROPPER) {
-				
+
 				player.sendMessage(prefix + ChatColor.GREEN + "Successfully created a Crafting Station!");
 				player.playSound(bLoc, Sound.BLOCK_ANVIL_USE, 1, 1);
-				
+
 			}
 		}
 	}
